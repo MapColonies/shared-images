5e261098dc2985f537c3c1724f0eefd66517c2fa
ellamartirosyan <ellamartirosyan@gmail.com>
2023-03-16 12:10:23 +0200

    fix: cors

--- qgis-server/copy-projects.mjs
+++ qgis-server/copy-projects.mjs
@@ -14,8 +14,12 @@ $.verbose = false;
     while (folder = regexp.exec(bucketList)) {
       const currentFolder = folder[0].replace('/','');
       console.log('*** Copying project folder of --> ', currentFolder);
-      const output = (await $`aws s3 cp --endpoint-url ${process.env.AWS_ENDPOINT_URL} s3://${process.env.AWS_BUCKET_NAME}/${dataType}/${currentFolder}/project /io/data/${currentFolder} --recursive`).stdout.trim();
-      console.log(output);
+      try {
+        const output = (await $`aws s3 cp --endpoint-url ${process.env.AWS_ENDPOINT_URL} s3://${process.env.AWS_BUCKET_NAME}/${dataType}/${currentFolder}/project /io/data/${currentFolder} --recursive`).stdout.trim();
+        console.log(output);
+      } catch (err) {
+        console.log(`\n!!!**** Error: 'aws s3 cp' failed for ${dataType} ****!!!\n`);
+      }
       await $`sed -i 's,{RAW_DATA_PROXY_URL},${process.env.RAW_DATA_PROXY_URL},g' /io/data/${currentFolder}/${currentFolder}.qgs`;
     } 
   })
